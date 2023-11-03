5530c5f8313eb9787ca0782410b680c99a52a038
Jonathan Cheng <chengjonathan2021@gmail.com>
2023-11-03 02:29:58 -0700

    Changed Player Hitbox

--- src/prefabs/Player.js
+++ src/prefabs/Player.js
@@ -2,7 +2,7 @@ class Player extends Phaser.Physics.Arcade.Sprite {
     constructor(scene, x, y, texture) {
         super(scene, x, y, texture);
         
-        this.setScale(.5, .5);
+        this.setScale(.7, .7);
         
         // Adding the player to the scene
         scene.add.existing(this);  // necessary for displaying the texture
@@ -11,7 +11,7 @@ class Player extends Phaser.Physics.Arcade.Sprite {
         // Player physics properties. Adjust the body settings here as needed
         this.setCollideWorldBounds(true); // Player cannot move out of the game world
 
-        this.body.setSize(this.width * 0.8, this.height * .2); 
+        this.body.setSize(this.width * 0.8, this.height * .1); 
         this.body.setOffset(this.width * 0.1, this.height * 0.1); 
 
         // Initialize player properties from global variables
